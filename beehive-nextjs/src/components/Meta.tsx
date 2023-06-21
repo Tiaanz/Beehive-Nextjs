b87390000db1deca18e31cbc78283881765174ad
Tiaanz <jwtian126@gmail.com>
2023-06-22 10:43:52 +1200

    added favicon

--- beehive-nextjs/src/components/Meta.tsx
+++ beehive-nextjs/src/components/Meta.tsx
@@ -13,7 +13,7 @@ const Meta: FC<MetaProps> = ({ title}) => {
       <meta name="keywords" content="ECE relievers, ECE teachers, Early Childhood Centres, Childcare" />
       <meta name="description" content="Beehive joins early childcare relief teachers with early childcare centres." />
       <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/logo.png" />
       <title>{title}</title>
     </Head>
   )
